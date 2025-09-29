cd5e9af93f074ec676d1a79ac82170e5a6a124f8
Simauniha <2005.simaunihao@email.com>
2025-09-29 16:31:32 +0530

    add

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express=require('express');
-
+const cors=require("cors")
 const app=express();
 require('dotenv').config();
 const DbConnect=require('./Config/dbConfig');
 
 const router=require('./Router/router');
 app.use(express.json());
+app.use(cors([]))
 
 app.use(express.urlencoded({ extended: true }));
 
